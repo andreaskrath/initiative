6d218984732071976f0e504ae3038d5867f8b1b7
Andreas Krath <andreas.krath+github@gmail.com>
2025-04-14 12:36:15 +0200

    Router: Add initial index endpoint
    Might swap to return `Response` with `into_response` call - as this
    makes a lot of things easier down the line.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+use axum::{Router, response::Html, routing::get};
+use tokio::net::TcpListener;
+
 fn main() {
     async_main();
 }
 
 #[tokio::main]
-async fn async_main() {}
+async fn async_main() {
+    let app = Router::new().route("/", get(index));
+
+    let listener = TcpListener::bind("127.0.0.1:8080")
+        .await
+        .expect("failed to bind to port 8080");
+    axum::serve(listener, app)
+        .await
+        .expect("failed to start server on port 8080");
+}
+
+async fn index() -> Html<&'static str> {
+    Html("<h1>Hello, world!</h1>")
+}
