0bd00c09c0da801a0d7d5bb93b506fc697699a9d
Andreas Krath <andreas.krath+github@gmail.com>
2025-04-15 20:59:07 +0200

    Tailwind: Setup and add example

--- view/src/App.svelte
+++ view/src/App.svelte
@@ -1,25 +1,22 @@
 <script lang="ts">
-  import Counter from './lib/Counter.svelte'
+  import Counter from "./lib/Counter.svelte";
+  import "./app.css";
 </script>
 
 <main>
   <h1>Combat Tracker</h1>
 
-  <div class="card">
+  <div class="flex content-center items-center place-content-center">
     <Counter />
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
-
-<style>
-  .read-the-docs {
-    color: #888;
-  }
-</style>
