e864dc9c50bf998b542428cd0aca4f73b7f49cf6
Andreas Krath <andreas.krath+github@gmail.com>
2025-06-25 23:15:38 +0200

    Monsters: Preview button

--- view/src/lib/Monsters.svelte
+++ view/src/lib/Monsters.svelte
@@ -1,1 +1,14 @@
-<h1 class="dnd-font color-text title">Monsters</h1>
+<script lang="ts">
+  import Button from "./components/Button.svelte";
+
+  function handleButtonClick(event: MouseEvent) {
+    event.preventDefault();
+    console.log("Hello");
+  }
+</script>
+
+<div>
+  <h1 class="dnd-font color-text title">Monsters</h1>
+
+  <Button title="Create New Monster" onClick={handleButtonClick} />
+</div>
