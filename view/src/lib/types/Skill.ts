b3f86512b7ede74d6edb9720bceba939a26232a4
Andreas Krath <andreas.krath+github@gmail.com>
2025-06-28 19:20:06 +0200

    Types: Add `Skill` and `Skills`

---
+++ view/src/lib/types/Skill.ts
@@ -0,0 +1,22 @@
+export enum Skill {
+  Athletics = "Athletics",
+  Acrobatics = "Acrobatics",
+  SleightOfHand = "Sleight of Hand",
+  Stealth = "Stealth",
+  Arcana = "Arcana",
+  History = "History",
+  Investigation = "Investigation",
+  Nature = "Nature",
+  Religion = "Religion",
+  AnimalHandling = "Animal Handling",
+  Insight = "Insight",
+  Medicine = "Medicine",
+  Perception = "Perception",
+  Survival = "Survival",
+  Deception = "Deception",
+  Intimidation = "Intimidation",
+  Performance = "Performance",
+  Persuasion = "Persuasion",
+}
+
+export const Skills: Skill[] = Object.values(Skill);
