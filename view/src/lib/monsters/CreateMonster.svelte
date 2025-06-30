59d35e7080d1b0cae122b316140149c2025e728a
Andreas Krath <andreas.krath+github@gmail.com>
2025-06-30 21:33:52 +0200

    CreateMonster: Make all state reactive
    Bind to a `$state` variable with an inline object for the monster.
    
    Next steps is splitting the different actions out into:
    - Regular action
    - Attack action
    - Recharge action
    - Bonus action
    - Reaction action
    - Legendary action
    - Lair action
    
    Also remember to add conditional immunity to defensive panel.

--- view/src/lib/monsters/CreateMonster.svelte
+++ view/src/lib/monsters/CreateMonster.svelte
@@ -9,54 +9,126 @@
   import { Sight, Sights } from "../types/Sight";
   import { Language, Languages } from "../types/Language";
   import { Skill, Skills } from "../types/Skill";
+  import { Attribute, Attributes } from "../types/Attribute";
 
-  let traits: { name: string | null; description: string | null }[] = [];
+  function AttributesFactory() {
+    return Attributes.reduce(
+      (record, attribute) => {
+        record[attribute] = null;
+        return record;
+      },
+      {} as Record<Attribute, number | null>,
+    );
+  }
+
+  function DamageTypesFactory() {
+    return DamageTypes.reduce(
+      (record, damageType) => {
+        record[damageType] = false;
+        return record;
+      },
+      {} as Record<DamageType, boolean>,
+    );
+  }
+
+  function LanguagesFactory() {
+    return Languages.reduce(
+      (record, language) => {
+        record[language] = false;
+        return record;
+      },
+      {} as Record<Language, boolean>,
+    );
+  }
+
+  function SkillsFactory() {
+    return Skills.reduce(
+      (record, skill) => {
+        record[skill] = null;
+        return record;
+      },
+      {} as Record<Skill, number | null>,
+    );
+  }
+
+  // Need to use a inline object, because classes are broken as reactive components.
+  let monster = $state({
+    name: null as string | null,
+    challengeRating: null as number | null,
+    xp: null as number | null,
+    proficiencyBonus: null as number | null,
+    size: Size.Medium,
+    monsterType: MonsterType.Beast,
+    species: null as string | null,
+    alignment: Alignment.Any,
+    attributes: AttributesFactory(),
+    hitPoints: null as number | null,
+    rollableHitPoints: null as string | null,
+    armorClass: null as number | null,
+    armorType: null as string | null,
+    savingThrows: AttributesFactory(),
+    damageResistances: DamageTypesFactory(),
+    damageImmunities: DamageTypesFactory(),
+    visions: [] as { type: Sight; range: number | null }[],
+    languages: LanguagesFactory(),
+    skills: SkillsFactory(),
+    traits: [] as { name: string | null; description: string | null }[],
+  });
+
+  function addVision(event: MouseEvent) {
+    monster.visions = [
+      ...monster.visions,
+      { type: Sight.Darkvision, range: null },
+    ];
+    event.preventDefault();
+  }
+
+  function removeVision(visionToRemove: { type: Sight; range: number | null }) {
+    return function (event: MouseEvent) {
+      monster.visions = monster.visions.filter(
+        (vision) => vision !== visionToRemove,
+      );
+      event.preventDefault();
+    };
+  }
 
   function addTrait(event: MouseEvent) {
-    traits = [...traits, { name: null, description: null }];
+    monster.traits = [...monster.traits, { name: null, description: null }];
+
+    event.preventDefault();
   }
 
   function removeTrait(traitToRemove: {
     name: string | null;
     description: string | null;
   }) {
-    traits = traits.filter((trait) => trait !== traitToRemove);
-
     return function (event: MouseEvent) {
+      monster.traits = monster.traits.filter(
+        (trait) => trait !== traitToRemove,
+      );
       event.preventDefault();
     };
   }
 
-  let visions: { type: Sight; range: number }[] = [];
-
-  function addVision(event: MouseEvent) {
-    visions = [...visions, { type: Sight.Darkvision, range: 0 }];
-
-    event.preventDefault();
-  }
-
-  function removeVision(visionToRemove: { type: Sight; range: number }) {
-    // This comparison is by reference, ensuring it removes the correct entry.
-    visions = visions.filter((vision) => vision !== visionToRemove);
-
-    return function (event: MouseEvent) {
-      event.preventDefault();
-    };
-  }
-
-  // Leave for now - just an example of how to pre-select default in select inputs.
-  let monsterSize = Size.Medium;
+  // Comment in for console.logs of each state update.
+  // $inspect(monster);
 </script>
 
 <div class="py-2"></div>
-<form class="w-[1200px] mx-auto space-y-8 px-2">
+<form class="w-[1200px] mx-auto space-y-2 px-2">
+  <!-- Basic Information -->
   <h2 class="h2">Basic Information</h2>
 
   <!-- Name, Challenge, XP & Proficiency-->
   <div class="input-group grid-cols-16">
     <!-- Name -->
     <div class="ig-cell preset-tonal col-span-1">Name</div>
-    <input class="ig-input col-span-6" type="text" placeholder="Goblin" />
+    <input
+      class="ig-input col-span-6"
+      type="text"
+      placeholder="Goblin"
+      bind:value={monster.name}
+    />
 
     <!-- Challenge Rating -->
     <div class="ig-cell preset-tonal col-span-2">Challenge</div>
@@ -64,6 +136,7 @@
       class="ig-input col-span-1 text-center no-spinner"
       type="number"
       placeholder="0.5"
+      bind:value={monster.challengeRating}
     />
 
     <!-- Experience Points -->
@@ -72,6 +145,7 @@
       class="ig-input col-span-2 text-center"
       type="number"
       placeholder="100"
+      bind:value={monster.xp}
     />
 
     <!-- Proficiency Bonus -->
@@ -80,6 +154,7 @@
       class="ig-input col-span-1 text-center"
       type="number"
       placeholder="2"
+      bind:value={monster.proficiencyBonus}
     />
   </div>
 
@@ -87,15 +162,18 @@
   <div class="input-group grid-cols-32">
     <!-- Size -->
     <div class="ig-cell preset-tonal col-span-2">Size</div>
-    <select class="ig-select col-span-5 text-center" bind:value={monsterSize}>
+    <select class="ig-select col-span-5 text-center" bind:value={monster.size}>
       {#each Sizes as size}
         <option>{size}</option>
       {/each}
     </select>
 
-    <!-- Type -->
+    <!-- Monster Type -->
     <div class="ig-cell preset-tonal col-span-2">Type</div>
-    <select class="ig-select col-span-6 text-center">
+    <select
+      class="ig-select col-span-6 text-center"
+      bind:value={monster.monsterType}
+    >
       {#each MonsterTypes as monsterType}
         <option>{monsterType}</option>
       {/each}
@@ -103,51 +181,45 @@
 
     <!-- Species -->
     <div class="ig-cell preset-tonal col-span-2">Species</div>
-    <input class="ig-input col-span-6" type="text" placeholder="Goblinoid" />
+    <input
+      class="ig-input col-span-6"
+      type="text"
+      placeholder="Goblinoid"
+      bind:value={monster.species}
+    />
 
     <!-- Alignment -->
     <div class="ig-cell preset-tonal col-span-3">Alignment</div>
-    <select class="ig-select col-span-6 text-center">
+    <select
+      class="ig-select col-span-6 text-center"
+      bind:value={monster.alignment}
+    >
       {#each Alignments as alignment}
         <option>{alignment}</option>
       {/each}
     </select>
   </div>
 
+  <!-- Attributes -->
   <hr class="hr" />
   <h2 class="h2">Attributes</h2>
 
-  <!-- Attributes -->
   <div class="input-group grid-cols-18">
-    <!-- Strength -->
-    <div class="ig-cell preset-tonal col-span-2">Strength</div>
-    <input class="ig-input text-center" type="number" placeholder="13" />
-
-    <!-- Dexterity -->
-    <div class="ig-cell preset-tonal col-span-2">Dexterity</div>
-    <input class="ig-input text-center" type="number" placeholder="12" />
-
-    <!-- Constitution -->
-    <div class="ig-cell preset-tonal col-span-2">Constitution</div>
-    <input class="ig-input text-center" type="number" placeholder="12" />
-
-    <!-- Intelligence -->
-    <div class="ig-cell preset-tonal col-span-2">Intelligence</div>
-    <input class="ig-input text-center" type="number" placeholder="10" />
-
-    <!-- Wisdom -->
-    <div class="ig-cell preset-tonal col-span-2">Wisdom</div>
-    <input class="ig-input text-center" type="number" placeholder="10" />
-
-    <!-- Charisma -->
-    <div class="ig-cell preset-tonal col-span-2">Charisma</div>
-    <input class="ig-input text-center" type="number" placeholder="9" />
+    {#each Attributes as attribute}
+      <div class="ig-cell preset-tonal col-span-2">{attribute}</div>
+      <input
+        class="ig-input text-center"
+        type="number"
+        placeholder="13"
+        bind:value={monster.attributes[attribute]}
+      />
+    {/each}
   </div>
 
+  <!-- Defensive -->
   <hr class="hr" />
   <h2 class="h2">Defensive</h2>
 
-  <!-- Hit Points, Rollable HP, Armor Class & Armor Type -->
   <div class="input-group grid-cols-16">
     <!-- Hit Points -->
     <div class="ig-cell preset-tonal col-span-2">Hit Points</div>
@@ -155,6 +227,7 @@
       class="ig-input col-span-1 text-center"
       type="number"
       placeholder="11"
+      bind:value={monster.hitPoints}
     />
 
     <!-- Rollable Hit Points -->
@@ -165,6 +238,7 @@
       class="ig-input text-center col-span-2"
       type="text"
       placeholder="2d8 +2"
+      bind:value={monster.rollableHitPoints}
     />
 
     <!-- Armor Class -->
@@ -173,6 +247,7 @@
       class="ig-input text-center col-span-1"
       type="number"
       placeholder="18"
+      bind:value={monster.armorClass}
     />
 
     <!-- Armor Type -->
@@ -181,45 +256,36 @@
       class="ig-input col-span-5"
       type="text"
       placeholder="chain mail, shield"
+      bind:value={monster.armorType}
     />
   </div>
 
+  <!-- Saving Throws -->
   <h6 class="h6">Saving Throws</h6>
   <div class="input-group grid-cols-18">
-    <!-- Strength -->
-    <div class="ig-cell preset-tonal col-span-2">Strength</div>
-    <input class="ig-input text-center" type="number" placeholder="3" />
-
-    <!-- Dexterity -->
-    <div class="ig-cell preset-tonal col-span-2">Dexterity</div>
-    <input class="ig-input text-center" type="number" placeholder="2" />
-
-    <!-- Constitution -->
-    <div class="ig-cell preset-tonal col-span-2">Constitution</div>
-    <input class="ig-input text-center" type="number" placeholder="" />
-
-    <!-- Intelligence -->
-    <div class="ig-cell preset-tonal col-span-2">Intelligence</div>
-    <input class="ig-input text-center" type="number" placeholder="" />
-
-    <!-- Wisdom -->
-    <div class="ig-cell preset-tonal col-span-2">Wisdom</div>
-    <input class="ig-input text-center" type="number" placeholder="" />
-
-    <!-- Charisma -->
-    <div class="ig-cell preset-tonal col-span-2">Charisma</div>
-    <input class="ig-input text-center" type="number" placeholder="" />
+    {#each Attributes as attribute}
+      <div class="ig-cell preset-tonal col-span-2">{attribute}</div>
+      <input
+        class="ig-input text-center"
+        type="number"
+        bind:value={monster.savingThrows[attribute]}
+      />
+    {/each}
   </div>
 
+  <!-- Damage Resistances -->
   <h6 class="h6">Damage Resistances</h6>
 
-  <!-- Damage Resistances -->
   <div class="input-group grid-cols-4 border-none outline-none py-2 px-2">
     {#each DamageTypes as damageType}
       <label
         class="flex items-center space-x-2 space-y-2 border-none outline-none"
       >
-        <input class="checkbox" type="checkbox" />
+        <input
+          class="checkbox"
+          type="checkbox"
+          bind:checked={monster.damageResistances[damageType]}
+        />
         <p>{damageType}</p>
       </label>
     {/each}
@@ -233,12 +299,17 @@
       <label
         class="flex items-center space-x-2 space-y-2 border-none outline-none"
       >
-        <input class="checkbox" type="checkbox" />
+        <input
+          class="checkbox"
+          type="checkbox"
+          bind:checked={monster.damageImmunities[damageType]}
+        />
         <p>{damageType}</p>
       </label>
     {/each}
   </div>
 
+  <!-- Vision & Perception -->
   <hr class="hr" />
   <div class="flex justify-between">
     <h2 class="h2">Senses</h2>
@@ -249,7 +320,6 @@
     >
   </div>
 
-  <!-- Vision & Perception -->
   <div class="input-group grid-cols-6">
     <div class="ig-cell preset-tonal col-span-3">Passive Perception</div>
     <input
@@ -258,7 +328,7 @@
       placeholder="10"
     />
 
-    {#each visions as vision}
+    {#each monster.visions as vision}
       <input
         class="ig-input text-center col-span-1"
         type="number"
@@ -278,31 +348,46 @@
     {/each}
   </div>
 
+  <!-- Languages -->
   <hr class="hr" />
   <h2 class="h2">Languages</h2>
 
-  <!-- Languages -->
   <div class="input-group grid-cols-4 border-none outline-none py-2 px-2">
     {#each Languages as language}
       <label
         class="flex items-center space-x-2 space-y-2 border-none outline-none"
       >
-        <input class="checkbox" type="checkbox" />
+        <input
+          class="checkbox"
+          type="checkbox"
+          bind:checked={monster.languages[language]}
+        />
         <p>{language}</p>
       </label>
     {/each}
   </div>
 
+  <!-- Skills -->
+  <hr class="hr" />
   <h2 class="h2">Skills</h2>
 
-  <!-- Skills -->
   <div class="input-group gap-y-1 px-1 py-1 grid-cols-6">
-    {#each Skills as skill}
+    {#each Skills as skill, index}
       <div class="ig-cell preset-tonal col-span-1">{skill}</div>
-      <input class="ig-input text-center col-span-1" type="number" />
+      <input
+        class="ig-input text-center col-span-1"
+        type="number"
+        bind:value={monster.skills[skill]}
+      />
+      <!-- This is nasty, but draws the horizontal rulers correctly. -->
+      {#if index !== 0 && index !== Skills.length - 1 && index % 3 === 2}
+        <hr class="hr col-span-6" />
+      {/if}
     {/each}
   </div>
 
+  <!-- Traits -->
+  <hr class="hr" />
   <div class="flex justify-between">
     <h2 class="h2">Traits</h2>
     <button
@@ -312,9 +397,8 @@
     >
   </div>
 
-  <!-- Traits -->
   <div class="input-group grid-cols-8">
-    {#each traits as trait}
+    {#each monster.traits as trait}
       <!-- Name -->
       <div class="ig-cell preset-tonal col-span-1">Name</div>
       <input
@@ -340,6 +424,23 @@
       <hr class="col-span-8" />
     {/each}
   </div>
+
+  <hr class="hr" />
+  <h2 class="h2">Actions</h2>
+
+  <!-- Actions -->
+  <div class="input-group grid-cols-8">
+    <!-- Regular Actions -->
+    <!-- Attack Actions -->
+    <!-- Recharge Actions -->
+    <!-- Bonus Actions -->
+    <!-- Reaction Actions -->
+    <!-- Legendary Actions -->
+  </div>
+
+  <!-- <button type="button" class="btn" onclick={(e) => $inspect(monster)} -->
+  <!--   >Save Monster</button -->
+  <!-- > -->
 
   <div class="h-[100px]"></div>
 </form>
