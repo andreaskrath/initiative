be74fd4b7d2a7328ed32156a25fabfab5efdba89
Andreas Krath <andreas.krath+github@gmail.com>
2025-07-03 10:05:36 +0200

    Components: Add Input

---
+++ view/src/lib/components/Input.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  let {
+    label,
+    value = $bindable(),
+    type,
+    placeholder,
+    labelSize,
+    inputSize,
+    center = false,
+  }: {
+    label: string;
+    value: string | number | null;
+    type: "text" | "number";
+    placeholder: string;
+    labelSize: number;
+    inputSize: number;
+    center?: boolean;
+  } = $props();
+</script>
+
+<div class="ig-cell preset-tonal col-span-{labelSize}">{label}</div>
+<input
+  class="ig-input col-span-{inputSize} {type === 'number'
+    ? 'text-center no-spinner'
+    : ''} {center ? 'text-center' : ''}"
+  {type}
+  {placeholder}
+  bind:value
+/>
