7f70bb0199c9f6d6d6ae167e6eb91b3f6622299f
Andreas Krath <andreas.krath+github@gmail.com>
2025-07-04 00:32:09 +0200

    Navbar: Change style to custom appearance with no icons

--- view/src/App.svelte
+++ view/src/App.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
   import "./app.css";
-  import Monsters from "./lib/monsters/Monsters.svelte";
-  import Encounters from "./lib/encounters/Encounters.svelte";
-  import NotFound from "./lib/NotFound.svelte";
-  import Settings from "./lib/settings/Settings.svelte";
+  import Monsters from "$lib/monsters/Monsters.svelte";
+  import Encounters from "$lib/encounters/Encounters.svelte";
+  import NotFound from "$lib/NotFound.svelte";
+  import Settings from "$lib/settings/Settings.svelte";
+  import Spells from "$lib/spells/Spells.svelte";
   import Navbar from "$components/Navbar.svelte";
   import type { Component } from "svelte";
 
-  const pages: Component[] = [Monsters, Encounters, Settings];
+  const pages: Component[] = [Monsters, Encounters, Spells, Settings];
   let currentPage: number = $state(0);
   const PageComponent = $derived(pages[currentPage] || NotFound);
 </script>
 
-<Navbar bind:item={currentPage} />
+<Navbar
+  bind:currentItem={currentPage}
+  items={["Monsters", "Encounters", "Spells", "Settings"]}
+/>
 <main>
   <div class="flex content-center items-center place-content-center">
     <PageComponent />
