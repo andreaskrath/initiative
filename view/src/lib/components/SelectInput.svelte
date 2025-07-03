744b266fc3164449ecb952f030fb756bf4a73189
Andreas Krath <andreas.krath+github@gmail.com>
2025-07-03 10:05:43 +0200

    Components: Add SelectInput

---
+++ view/src/lib/components/SelectInput.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  let {
+    title,
+    value = $bindable(),
+    items,
+    labelSize,
+    inputSize,
+  }: {
+    title: string;
+    value: string | null;
+    items: string[];
+    labelSize: number;
+    inputSize: number;
+  } = $props();
+</script>
+
+<div class="ig-cell preset-tonal col-span-{labelSize}">{title}</div>
+<select class="ig-select col-span-{inputSize} text-center" bind:value>
+  {#each items as item}
+    <option>{item}</option>
+  {/each}
+</select>
