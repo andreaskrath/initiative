c8084311da1db5be9d89ea3c108263fd20abbdf6
Andreas Krath <andreas.krath+github@gmail.com>
2025-07-04 23:14:32 +0200

    Components: Add Checkbox

---
+++ view/src/lib/components/Checkbox.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let {
+    label,
+    value = $bindable(),
+  }: {
+    label: string;
+    value: boolean;
+  } = $props();
+</script>
+
+<label class="flex items-center space-x-2 border-none outline-none col-span-1">
+  <input class="checkbox" type="checkbox" bind:checked={value} />
+  <p>{label}</p>
+</label>
