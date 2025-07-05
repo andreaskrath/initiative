5d17f20f76ff2e6617b26193c2e3df552186be86
Andreas Krath <andreas.krath+github@gmail.com>
2025-07-05 17:12:16 +0200

    Components: Make Input based on shadcn

--- view/src/lib/components/Input.svelte
+++ view/src/lib/components/Input.svelte
@@ -1,34 +1,27 @@
 <script lang="ts">
+  import { Input } from "$components/ui/input/index";
+  import { Label } from "$components/ui/label/index";
+
   let {
     label,
     value = $bindable(),
     type,
     placeholder,
-    labelSize,
-    inputSize,
-    center = false,
+    columns,
+    center,
   }: {
     label: string;
-    value: string | number | null;
+    value: string | number | undefined;
     type: "text" | "number";
     placeholder: string;
-    labelSize: number;
-    inputSize: number;
+    columns: number;
     center?: boolean;
   } = $props();
 </script>
 
 <!-- Have to use inline CSS because dynamic tailwind classes are not picked up by compiler -->
 
-<div class="ig-cell preset-tonal" style="grid-column: span {labelSize}">
-  {label}
+<div class="flex w-full flex-col gap-1.5" style="grid-column: span {columns}">
+  <Label class="ml-1"><strong>{label}</strong></Label>
+  <Input class={center ? "text-center" : ""} {type} {placeholder} bind:value />
 </div>
-<input
-  class="ig-input {type === 'number' ? 'text-center no-spinner' : ''} {center
-    ? 'text-center'
-    : ''}"
-  style="grid-column: span {inputSize}"
-  {type}
-  {placeholder}
-  bind:value
-/>
