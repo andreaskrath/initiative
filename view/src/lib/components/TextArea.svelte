52cb61c4d34d28eaba49c2bad398655f62c0b9e3
Andreas Krath <andreas.krath+github@gmail.com>
2025-07-06 21:10:02 +0200

    Components: Add TextArea - based on shadcn textarea

---
+++ view/src/lib/components/TextArea.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { Label } from "$components/ui/label/index";
+  import { Textarea } from "$components/ui/textarea/index";
+
+  let {
+    label,
+    value = $bindable(),
+    placeholder,
+    id,
+    columns,
+  }: {
+    label: string;
+    value?: string;
+    placeholder: string;
+    id: string;
+    columns: number;
+  } = $props();
+</script>
+
+<div class="space-y-2" style="grid-column: span {columns}">
+  <Label for={id}>{label}</Label>
+  <Textarea bind:value {placeholder} {id} class="w-full" />
+</div>
