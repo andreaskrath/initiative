d38b513e0a65bdad8baae883f051e7481fde84b6
Andreas Krath <andreas.krath+github@gmail.com>
2025-07-24 22:40:19 +0200

    View/Types: Change Spell nullable properties to undefined
    Fits with the new structure and standard, also makes bindings easier
    generally.

--- view/src/lib/types/Spell.ts
+++ view/src/lib/types/Spell.ts
@@ -4,39 +4,39 @@ import { Class, SpellcastingClasses } from "$types/Class";
 import { RecordFactory } from "$utils/factories";
 
 export type Spell = {
-  name: string | null;
-  school: MagicSchool | null;
-  level: SpellLevel | null;
-  castingTime: string | null;
+  name?: string;
+  school?: MagicSchool;
+  level?: SpellLevel;
+  castingTime?: string;
   verbal: boolean;
   somatic: boolean;
-  material: string | null;
+  material?: string;
   materialConsumed: boolean;
   ritual: boolean;
   concentration: boolean;
-  duration: string | null;
-  range: string | null;
-  area: string | null;
-  shape: string | null;
+  duration?: string;
+  range?: string;
+  area?: string;
+  shape?: string;
   classes: Record<Class, boolean>;
 };
 
 export const SpellActions = {
   EmptySpell: (): Spell => ({
-    name: null,
-    school: null,
-    level: null,
-    castingTime: null,
+    name: undefined,
+    school: undefined,
+    level: undefined,
+    castingTime: undefined,
     verbal: false,
     somatic: false,
-    material: null,
+    material: undefined,
     materialConsumed: false,
     ritual: false,
     concentration: false,
-    duration: null,
-    range: null,
-    area: null,
-    shape: null,
+    duration: undefined,
+    range: undefined,
+    area: undefined,
+    shape: undefined,
     classes: RecordFactory(SpellcastingClasses, false),
   }),
 };
