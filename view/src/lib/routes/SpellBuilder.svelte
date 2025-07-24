ea9f2fe7c25df691722357844c6c4f609cccb17f
Andreas Krath <andreas.krath+github@gmail.com>
2025-07-24 22:41:12 +0200

    View/SpellBuilder: Add full layout
    Need to implement functionality still (and button for creating a spell),
    but otherwise full layout is "done" - I might still work around with
    styling for some things, like the very wide Shape and Area select lists.

--- view/src/lib/routes/SpellBuilder.svelte
+++ view/src/lib/routes/SpellBuilder.svelte
@@ -1,238 +1,194 @@
-<!--     <Checkbox -->
-<!--       label="Materials consumed" -->
-<!--       bind:value={spell.materialConsumed} -->
-<!--       columns={3} -->
-<!--       center={true} -->
-<!--     /> -->
-<!--   </div> -->
-<!---->
-<!--   <h6 class="h6">Class Restrictions</h6> -->
-<!--   <CheckboxGroup -->
-<!--     items={SpellcastingClasses} -->
-<!--     bind:checkedItems={spell.classes} -->
-<!--     columns={3} -->
-<!--   /> -->
-<!-- </form> -->
-
 <script lang="ts">
-  let props = $props();
+  import { Areas } from "$types/Area";
+  import { CastingTimes } from "$types/CastingTime";
+  import { Durations } from "$types/Duration";
+  import { MagicSchools } from "$types/MagicSchool";
+  import { Ranges } from "$types/Range";
+  import { Shapes } from "$types/Shape";
+  import { SpellActions } from "$types/Spell";
+  import { SpellLevels } from "$types/SpellLevel";
+  import { SpellcastingClasses } from "$types/Class";
+
+  import { LabelValueFactory } from "$utils/factories";
+
+  import Combobox from "$components/Combobox.svelte";
+  import Container from "$components/Container.svelte";
+  import Input from "$components/Input.svelte";
+  import Label from "$components/Label.svelte";
+  import Select from "$components/Select.svelte";
+  import TextArea from "$lib/components/TextArea.svelte";
+  import Title from "$components/Title.svelte";
+  import Toggle from "$components/Toggle.svelte";
+
+  let spell = $state(SpellActions.EmptySpell());
+  let usesMaterials = $state(false);
+
+  const areas = LabelValueFactory(Areas);
+  const castingTimes = LabelValueFactory(CastingTimes);
+  const durations = LabelValueFactory(Durations);
+  const ranges = LabelValueFactory(Ranges);
+  const schools = LabelValueFactory(MagicSchools);
+  const shapes = LabelValueFactory(Shapes);
+  const spellLevels = LabelValueFactory(SpellLevels);
 </script>
 
-<!-- <script lang="ts"> -->
-<!--   import Checkbox from "$components/Checkbox.svelte"; -->
-<!--   import CheckboxGroup from "$components/CheckboxGroup.svelte"; -->
-<!--   import Input from "$components/Input.svelte"; -->
-<!--   import SelectInput from "$components/SelectInput.svelte"; -->
-<!--   import { Areas } from "$types/Area"; -->
-<!--   import { CastingTimes } from "$types/CastingTime"; -->
-<!--   import { Durations } from "$types/Duration"; -->
-<!--   import { MagicSchools } from "$types/MagicSchool"; -->
-<!--   import { Ranges } from "$types/Range"; -->
-<!--   import { Shapes } from "$types/Shape"; -->
-<!--   import { Spell } from "$types/Spell.svelte"; -->
-<!--   import { SpellLevels } from "$types/SpellLevel"; -->
-<!--   import { SpellcastingClasses } from "$types/Class"; -->
-<!---->
-<!--   let spell = new Spell(); -->
-<!---->
-<!--   type ComboboxData = { label: string; value: string }; -->
-<!---->
-<!--   const castingTimesComboboxData: ComboboxData[] = CastingTimes.map( -->
-<!--     (castingTime) => ({ -->
-<!--       label: castingTime, -->
-<!--       value: castingTime, -->
-<!--     }), -->
-<!--   ); -->
-<!--   let selectedCastingTime: string[] = $state([]); -->
-<!--   let customCastingTime: string = $state(""); -->
-<!---->
-<!--   const durationComboboxData: ComboboxData[] = Durations.map((duration) => ({ -->
-<!--     label: duration, -->
-<!--     value: duration, -->
-<!--   })); -->
-<!--   let selectedDuration: string[] = $state([]); -->
-<!--   let customDuration: string = $state(""); -->
-<!---->
-<!--   const rangeComboboxData: ComboboxData[] = Ranges.map((range) => ({ -->
-<!--     label: range, -->
-<!--     value: range, -->
-<!--   })); -->
-<!--   let selectedRange: string[] = $state([]); -->
-<!--   let customRange: string = $state(""); -->
-<!---->
-<!--   const areaComboboxData: ComboboxData[] = Areas.map((area) => ({ -->
-<!--     label: area, -->
-<!--     value: area, -->
-<!--   })); -->
-<!--   let selectedArea: string[] = $state([]); -->
-<!--   let customArea: string = $state(""); -->
-<!---->
-<!--   const shapeComboboxData: ComboboxData[] = Shapes.map((shape) => ({ -->
-<!--     label: shape, -->
-<!--     value: shape, -->
-<!--   })); -->
-<!--   let selectedShape: string[] = $state([]); -->
-<!--   let customShape: string = $state(""); -->
-<!-- </script> -->
-<!---->
-<!-- <form class="space-y-2 py-2"> -->
-<!--   <!-- Basic Information --> -->
-<!--   <h2 class="h2">Basic Information</h2> -->
-<!--   <div class="input-group grid-cols-16"> -->
-<!--     <!-- Name --> -->
-<!--     <Input -->
-<!--       label="Name" -->
-<!--       bind:value={spell.name} -->
-<!--       type="text" -->
-<!--       placeholder="Fireball" -->
-<!--       labelSize={1} -->
-<!--       inputSize={8} -->
-<!--     /> -->
-<!---->
-<!--     <!-- School --> -->
-<!--     <SelectInput -->
-<!--       title="School" -->
-<!--       items={MagicSchools} -->
-<!--       bind:value={spell.school} -->
-<!--       labelSize={1} -->
-<!--       inputSize={3} -->
-<!--     /> -->
-<!---->
-<!--     <!-- Level --> -->
-<!--     <SelectInput -->
-<!--       title="Level" -->
-<!--       items={SpellLevels} -->
-<!--       bind:value={spell.level} -->
-<!--       labelSize={1} -->
-<!--       inputSize={2} -->
-<!--     /> -->
-<!---->
-<!--     <hr class="hr col-span-16" /> -->
-<!---->
-<!--     <!-- Casting Time --> -->
-<!--     <div class="ig-cell preset-tonal col-span-2">Casting Time</div> -->
-<!--     <div class="col-span-3"> -->
-<!--       <Combobox -->
-<!--         multiple={false} -->
-<!--         data={castingTimesComboboxData} -->
-<!--         value={selectedCastingTime} -->
-<!--         allowCustomValue={true} -->
-<!--         onValueChange={(details) => (selectedCastingTime = details.value)} -->
-<!--         onInputValueChange={(details) => -->
-<!--           (customCastingTime = details.inputValue)} -->
-<!--       > -->
-<!--         {#snippet item(option)} -->
-<!--           {option.label} -->
-<!--         {/snippet} -->
-<!--       </Combobox> -->
-<!--     </div> -->
-<!---->
-<!--     <!-- Duration --> -->
-<!--     <div class="ig-cell preset-tonal col-span-2">Duration</div> -->
-<!--     <div class="col-span-3"> -->
-<!--       <Combobox -->
-<!--         multiple={false} -->
-<!--         data={durationComboboxData} -->
-<!--         value={selectedDuration} -->
-<!--         allowCustomValue={true} -->
-<!--         onValueChange={(details) => (selectedDuration = details.value)} -->
-<!--         onInputValueChange={(details) => (customDuration = details.inputValue)} -->
-<!--       > -->
-<!--         {#snippet item(option)} -->
-<!--           {option.label} -->
-<!--         {/snippet} -->
-<!--       </Combobox> -->
-<!--     </div> -->
-<!---->
-<!--     <!-- Range --> -->
-<!--     <div class="ig-cell preset-tonal col-span-1">Range</div> -->
-<!--     <div class="col-span-3"> -->
-<!--       <Combobox -->
-<!--         multiple={false} -->
-<!--         data={rangeComboboxData} -->
-<!--         value={selectedRange} -->
-<!--         allowCustomValue={true} -->
-<!--         onValueChange={(details) => (selectedRange = details.value)} -->
-<!--         onInputValueChange={(details) => (customRange = details.inputValue)} -->
-<!--       > -->
-<!--         {#snippet item(option)} -->
-<!--           {option.label} -->
-<!--         {/snippet} -->
-<!--       </Combobox> -->
-<!--     </div> -->
-<!---->
-<!--     <!-- Ritual --> -->
-<!--     <Checkbox -->
-<!--       label="Ritual" -->
-<!--       bind:value={spell.ritual} -->
-<!--       columns={2} -->
-<!--       center={true} -->
-<!--     /> -->
-<!---->
-<!--     <hr class="hr col-span-16" /> -->
-<!---->
-<!--     <!-- Area --> -->
-<!--     <div class="ig-cell preset-tonal col-span-1">Area</div> -->
-<!--     <div class="col-span-4"> -->
-<!--       <Combobox -->
-<!--         multiple={false} -->
-<!--         data={areaComboboxData} -->
-<!--         value={selectedArea} -->
-<!--         allowCustomValue={true} -->
-<!--         onValueChange={(details) => (selectedArea = details.value)} -->
-<!--         onInputValueChange={(details) => (customArea = details.inputValue)} -->
-<!--       > -->
-<!--         {#snippet item(option)} -->
-<!--           {option.label} -->
-<!--         {/snippet} -->
-<!--       </Combobox> -->
-<!--     </div> -->
-<!---->
-<!--     <!-- Shape --> -->
-<!--     <div class="ig-cell preset-tonal col-span-1">Shape</div> -->
-<!--     <div class="col-span-3"> -->
-<!--       <Combobox -->
-<!--         multiple={false} -->
-<!--         data={shapeComboboxData} -->
-<!--         value={selectedShape} -->
-<!--         allowCustomValue={true} -->
-<!--         onValueChange={(details) => (selectedShape = details.value)} -->
-<!--         onInputValueChange={(details) => (customShape = details.inputValue)} -->
-<!--       > -->
-<!--         {#snippet item(option)} -->
-<!--           {option.label} -->
-<!--         {/snippet} -->
-<!--       </Combobox> -->
-<!--     </div> -->
-<!---->
-<!--     <!-- areaType: string | null; --> -->
-<!--   </div> -->
-<!---->
-<!--   <h6 class="h6">Requirements</h6> -->
-<!--   <div class="input-group grid-cols-16 px-2"> -->
-<!--     <!-- Verbal --> -->
-<!--     <Checkbox label="Verbal" bind:value={spell.verbal} columns={2} /> -->
-<!---->
-<!--     <!-- Somatic --> -->
-<!--     <Checkbox label="Somatic" bind:value={spell.somatic} columns={2} /> -->
-<!---->
-<!--     <!-- Concentration --> -->
-<!--     <Checkbox -->
-<!--       label="Concentration" -->
-<!--       bind:value={spell.concentration} -->
-<!--       columns={2} -->
-<!--     /> -->
-<!---->
-<!--     <!-- Material --> -->
-<!--     <Input -->
-<!--       label="Material" -->
-<!--       bind:value={spell.material} -->
-<!--       type="text" -->
-<!--       placeholder="a tiny ball of bat guano and sulfur" -->
-<!--       labelSize={2} -->
-<!--       inputSize={5} -->
-<!--     /> -->
-<!---->
-<!--     <!-- Material Consumed --> -->
+<div class="mx-auto mt-5 w-[1000px] space-y-5">
+  <!-- Basic Information -->
+  <Title variant="muted">Basic Spell Information</Title>
+  <div class="grid grid-cols-16 space-y-5 gap-x-2">
+    <!-- Name -->
+    <Container class="col-span-8">
+      <Label>Name</Label>
+      <Input bind:value={spell.name} placeholder="Fireball" type="text" />
+    </Container>
 
-<h1>Spell Builder</h1>
+    <!-- Spell Level -->
+    <Container class="col-span-4">
+      <Label>Spell Level</Label>
+      <Select
+        bind:value={spell.level}
+        placeholder="Select a spell level"
+        items={spellLevels}
+      />
+    </Container>
+
+    <!-- School of Magic -->
+    <Container class="col-span-4">
+      <Label>School of Magic</Label>
+      <Select
+        bind:value={spell.school}
+        placeholder="Select a school of magic"
+        items={schools}
+      />
+    </Container>
+
+    <!-- Casting Time -->
+    <Container class="col-span-4">
+      <Label>Casting Time</Label>
+      <Combobox
+        bind:value={spell.castingTime}
+        placeholder="Select a casting time"
+        items={castingTimes}
+      />
+    </Container>
+
+    <!-- Duration -->
+    <Container class="col-span-4">
+      <Label>Duration</Label>
+      <Combobox
+        bind:value={spell.duration}
+        placeholder="Select a duration"
+        items={durations}
+      />
+    </Container>
+
+    <!-- Range -->
+    <Container class="col-span-4">
+      <Label>Range</Label>
+      <Combobox
+        bind:value={spell.range}
+        placeholder="Select a range"
+        items={ranges}
+      />
+    </Container>
+
+    <!-- Ritual -->
+    <Container class="col-span-4 grid content-end">
+      <Toggle bind:checked={spell.ritual}>Ritual</Toggle>
+    </Container>
+
+    <!-- Shape -->
+    <Container class="col-span-8">
+      <Label>Shape</Label>
+      <Combobox
+        bind:value={spell.shape}
+        placeholder="Select a shape"
+        items={shapes}
+      />
+    </Container>
+
+    <!-- Area -->
+    <Container class="col-span-8">
+      <Label>Area</Label>
+      <Combobox
+        bind:value={spell.area}
+        placeholder="Select a area"
+        items={areas}
+      />
+    </Container>
+
+    <!-- Description -->
+    <Container class="col-span-16">
+      <Label>Description</Label>
+      <TextArea
+        bind:value={spell.description}
+        placeholder="A bright streak flashes from your pointing finger to a point you choose within range and then blossoms with a low roar into an explosion of flame. Each creature in a 20-foot-radius sphere centered on that point must make a Dexterity saving throw. A target takes 8d6 fire damage on a failed save, or half as much damage on a successful one.
+
+The fire spreads around corners. It ignites flammable objects in the area that aren't being worn or carried."
+      />
+    </Container>
+
+    <!-- At Higher Levels -->
+    <Container class="col-span-16">
+      <Label>At Higher Levels</Label>
+      <Input
+        bind:value={spell.atHigherLevels}
+        type="text"
+        placeholder="The damage increases by 1d6 for each slot level above 3rd."
+      />
+    </Container>
+  </div>
+
+  <!-- Requirements -->
+  <Title variant="muted">Requirements</Title>
+  <div class="grid grid-cols-16 space-y-5 gap-x-2">
+    <!-- Concentration -->
+    <Container class="col-span-2">
+      <Toggle bind:checked={spell.concentration}>Concentration</Toggle>
+    </Container>
+
+    <!-- Verbal -->
+    <Container class="col-span-2">
+      <Toggle bind:checked={spell.verbal}>Verbal</Toggle>
+    </Container>
+
+    <!-- Somatic -->
+    <Container class="col-span-2">
+      <Toggle bind:checked={spell.somatic}>Somatic</Toggle>
+    </Container>
+
+    <!-- Material -->
+    <Container class="col-span-2">
+      <Toggle bind:checked={usesMaterials}>Material</Toggle>
+    </Container>
+
+    {#if usesMaterials}
+      <!-- Material Consumed -->
+      <Container class="col-span-3">
+        <Toggle bind:checked={spell.materialConsumed}>Consumes Materials</Toggle
+        >
+      </Container>
+
+      <!-- Materials -->
+      <Container class="col-span-5">
+        <Input
+          bind:value={spell.material}
+          placeholder="a tiny ball of bat guano and sulfur"
+          type="text"
+        />
+      </Container>
+    {/if}
+  </div>
+
+  <!-- Class Restrictions -->
+  <Title variant="muted">Class Restrictions</Title>
+  <div class="grid grid-cols-3 space-y-2 gap-x-2">
+    {#each SpellcastingClasses as spellcastingClass}
+      <Container class="col-span-1">
+        <Toggle bind:checked={spell.classes[spellcastingClass]}>
+          {spellcastingClass}
+        </Toggle>
+      </Container>
+    {/each}
+  </div>
+</div>
