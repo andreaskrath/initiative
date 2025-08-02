61e5e4e630d9c6e30f7259afacb19708932aad80
Andreas Krath <andreas.krath+github@gmail.com>
2025-08-02 22:02:02 +0200

    View/Monster: Add service for creating a monster

---
+++ view/src/lib/monster/service.ts
@@ -0,0 +1,21 @@
+import { type Monster } from "$monster/types";
+import { Validate } from "$monster/validate";
+
+export const MonsterService = {
+  Create: async (monster: Monster): Promise<number | string[]> => {
+    let errors = await Validate(monster);
+    if (errors.length > 0) {
+      return errors;
+    }
+
+    const result = await fetch("/api/monsters/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(monster),
+    });
+
+    return result.status;
+  },
+};
