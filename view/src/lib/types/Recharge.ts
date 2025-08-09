dca8ded2ec69b2f24fbbc34e4de02c0f6936bb12
Andreas Krath <andreas.krath+github@gmail.com>
2025-08-09 10:33:16 +0200

    View/Types: Add new variants to recharge enum
    This is a loose enum, but I think the options are pretty common.

--- view/src/lib/types/Recharge.ts
+++ view/src/lib/types/Recharge.ts
@@ -2,6 +2,11 @@ export enum Recharge {
   FourToSix = "4-6",
   FiveToSix = "5-6",
   Six = "6",
+  ShortRest = "Short rest",
+  LongRest = "Long rest",
+  AnyRest = "Any rest",
+  Hour = "1 hour",
+  Day = "1 day",
 }
 
 export const Recharges: Recharge[] = Object.values(Recharge);
