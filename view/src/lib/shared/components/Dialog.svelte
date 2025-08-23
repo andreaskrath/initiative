523b23903113829ed3c1d53bd2974fb521c52e73
Andreas Krath <andreas.krath+github@gmail.com>
2025-08-23 23:57:35 +0200

    View/Components: Add generic dialog/modal
    Is made generic by the snippets.

---
+++ view/src/lib/shared/components/Dialog.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import * as Dialog from "$components/ui/dialog/index";
+  import type { Snippet } from "svelte";
+
+  let {
+    title,
+    description,
+    trigger,
+    content,
+    footer,
+  }: {
+    title: string;
+    description: string;
+    trigger: Snippet;
+    content: Snippet;
+    footer: Snippet;
+  } = $props();
+</script>
+
+<Dialog.Root>
+  <Dialog.Trigger>
+    {@render trigger()}
+  </Dialog.Trigger>
+  <Dialog.Content class="w-[800px] !max-w-none">
+    <Dialog.Header>
+      <Dialog.Title>{title}</Dialog.Title>
+      <Dialog.Description>
+        {description}
+      </Dialog.Description>
+    </Dialog.Header>
+    <div class="flex gap-5">
+      {@render content()}
+    </div>
+    <Dialog.Footer>
+      {@render footer()}
+    </Dialog.Footer>
+  </Dialog.Content>
+</Dialog.Root>
