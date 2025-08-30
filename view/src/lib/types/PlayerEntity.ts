1fd18afd0fdf55ad64bc74ce45250f3389ac2e36
Andreas Krath <andreas.krath+github@gmail.com>
2025-08-30 16:08:20 +0200

    View/Types: Add an actions object for PlayerEntity

--- view/src/lib/types/PlayerEntity.ts
+++ view/src/lib/types/PlayerEntity.ts
@@ -1,5 +1,17 @@
 import type { CombatEntity } from "./CombatEntity";
+import { ExhaustionLevel } from "./ExhaustionLevel";
 
 export interface PlayerEntity extends CombatEntity {
   type: "player";
 }
+
+export const PlayerEntityActions = {
+  EmptyPlayerEntity: (): PlayerEntity => ({
+    type: "player",
+    name: undefined,
+    initiative: undefined,
+    exhaustion_level: ExhaustionLevel.None,
+    concentration: false,
+    conditions: [],
+  }),
+};
