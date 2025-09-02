1e899cded69adb7fd1ca03343b63b2694c3f86ec
Andreas Krath <andreas.krath+github@gmail.com>
2025-09-02 21:31:53 +0200

    View/Components: Add Error component
    This should be used for showing validation/form errors for field inputs.

---
+++ view/src/lib/shared/components/Error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import CircleAlert from "@lucide/svelte/icons/circle-alert";
+  import * as Tooltip from "$components/ui/tooltip/index";
+
+  let { error }: { error: string } = $props();
+</script>
+
+<Tooltip.Provider>
+  <Tooltip.Root delayDuration={200}>
+    <Tooltip.Trigger class="absolute right-1 bottom-10 text-red-500">
+      <CircleAlert class="h-4 w-4" />
+    </Tooltip.Trigger>
+    <Tooltip.Content side="top" align="center">
+      <p>{error}</p>
+    </Tooltip.Content>
+  </Tooltip.Root>
+</Tooltip.Provider>
