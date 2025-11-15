a35df80c91f91f780c1bb688a7dfa05d7666b34c
Andreas Krath <andreas.krath+github@gmail.com>
2025-11-15 12:47:20 +0100

    crates/types: add `Class`
    might move to not be under spell in the future, unsure as of right now

---
+++ crates/types/src/spell/class.rs
@@ -0,0 +1,18 @@
+use strum::{Display, VariantArray};
+
+#[derive(Display, VariantArray, Clone, Copy)]
+pub enum Class {
+    Artificer,
+    Barbarian,
+    Bard,
+    Cleric,
+    Druid,
+    Fighter,
+    Monk,
+    Paladin,
+    Ranger,
+    Rogue,
+    Sorcerer,
+    Warlock,
+    Wizard,
+}
