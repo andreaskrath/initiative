ba44792263954a51474855c1abffc62c16b5adee
Andreas Krath <andreas.krath+github@gmail.com>
2025-11-30 18:21:35 +0100

    crates/app: add name, school and level input for `SpellForm`
    styling is super cooked right now, will fix that later on - some things
    are also dummy, like the errors
    
    will correct this in the future, still exploring how to do things at
    this point

--- crates/app/src/view/spell/form/message.rs
+++ crates/app/src/view/spell/form/message.rs
@@ -1,4 +1,8 @@
+use types::{MagicSchool, SpellLevel};
+
 #[derive(Debug, Clone)]
 pub enum SpellFormMessage {
     NameChanged(String),
+    SchoolSelected(MagicSchool),
+    LevelSelected(SpellLevel),
 }
--- crates/app/src/view/spell/form/mod.rs
+++ crates/app/src/view/spell/form/mod.rs
@@ -1,12 +1,15 @@
 mod message;
 
+use components::Field;
 use iced::{
     Element,
     Length::Fill,
     Task,
-    widget::{container, text},
+    widget::{container, pick_list, row, text_input},
 };
-use types::{FormMode, Spell};
+use strum::VariantArray;
+use tracing::debug;
+use types::{FormMode, MagicSchool, Spell, SpellLevel};
 
 use crate::{
     message::Message,
@@ -19,6 +22,9 @@ pub struct SpellForm {
     id: TabId,
     mode: FormMode,
     spell: Spell,
+    name: String,
+    school: Option<MagicSchool>,
+    level: Option<SpellLevel>,
 }
 
 impl SpellForm {
@@ -27,6 +33,9 @@ impl SpellForm {
             id,
             mode,
             spell: Spell::default(),
+            name: String::new(),
+            school: None,
+            level: None,
         }
     }
 }
@@ -46,17 +55,47 @@ impl TabContent for SpellForm {
     }
 
     fn update(&mut self, message: Self::ContentMessage) -> Task<Message> {
+        debug!("updating spell form with: {:?}", message);
+
         match message {
-            SpellFormMessage::NameChanged(name) => self.spell.name = name,
+            SpellFormMessage::NameChanged(name) => self.name = name,
+            SpellFormMessage::SchoolSelected(magic_school) => self.school = Some(magic_school),
+            SpellFormMessage::LevelSelected(spell_level) => self.level = Some(spell_level),
         }
 
         Task::none()
     }
 
     fn view(&self) -> Element<'_, Self::ContentMessage> {
-        container(text("Spell form"))
-            .width(Fill)
-            .center_x(Fill)
-            .into()
+        let name_input = text_input("Goblin", &self.name).on_input(SpellFormMessage::NameChanged);
+        let name = Field::new("Name", name_input)
+            .required(true)
+            .error("Bad bad boy");
+
+        let school_input = pick_list(
+            MagicSchool::VARIANTS,
+            self.school,
+            SpellFormMessage::SchoolSelected,
+        )
+        .placeholder("Select a school")
+        .width(Fill);
+        let school = Field::new("School", school_input)
+            .required(true)
+            .error("Bad bad boy");
+
+        let level_input = pick_list(
+            SpellLevel::VARIANTS,
+            self.level,
+            SpellFormMessage::LevelSelected,
+        )
+        .placeholder("Select a level")
+        .width(Fill);
+        let level = Field::new("School", level_input)
+            .required(true)
+            .error("Bad bad boy");
+
+        let first_row = row![name, school, level].spacing(10);
+
+        container(first_row).width(1200).into()
     }
 }
