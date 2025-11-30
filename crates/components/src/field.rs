aea8dd7c78119795ea76a51f16094c722cc0a4fa
Andreas Krath <andreas.krath+github@gmail.com>
2025-11-30 18:19:59 +0100

    crates/components: add `Field`
    might change the constraints on `T` at some point, but this works fine
    for now

---
+++ crates/components/src/field.rs
@@ -0,0 +1,71 @@
+use std::marker::PhantomData;
+
+use iced::{
+    Color, Element,
+    Length::Fill,
+    widget::{column, horizontal_space, row, text, tooltip, tooltip::Position},
+};
+
+use crate::{Icon, IconSize, icon};
+
+pub struct Field<'a, T, M>
+where
+    T: Into<Element<'a, M>>,
+{
+    label: &'a str,
+    input: T,
+    required: bool,
+    error: Option<&'a str>,
+    _marker: PhantomData<&'a M>,
+}
+
+impl<'a, T, M> Field<'a, T, M>
+where
+    T: Into<Element<'a, M>>,
+{
+    pub fn new(label: &'a str, input: T) -> Self {
+        Self {
+            label,
+            input,
+            required: false,
+            error: None,
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn required(mut self, required: bool) -> Self {
+        self.required = required;
+        self
+    }
+
+    pub fn error(mut self, error: &'a str) -> Self {
+        self.error = Some(error);
+        self
+    }
+}
+
+impl<'a, T, M> From<Field<'a, T, M>> for Element<'a, M>
+where
+    T: Into<Element<'a, M>>,
+{
+    fn from(field: Field<'a, T, M>) -> Self {
+        let icon = icon(Icon::Error, IconSize::Small);
+
+        let tooltip = field
+            .error
+            .map(|err| tooltip(icon, text(err), Position::Bottom));
+
+        let required: Element<'a, M> = if field.required {
+            text("*").color(Color::new(1.0, 0.0, 0.0, 1.0)).into()
+        } else {
+            horizontal_space().into()
+        };
+
+        let label = row![text(field.label), required, horizontal_space().width(Fill)];
+        let label = label.push_maybe(tooltip);
+
+        let input = field.input.into();
+
+        column![label, input].spacing(5).into()
+    }
+}
--- crates/components/src/lib.rs
+++ crates/components/src/lib.rs
@@ -1,5 +1,7 @@
 mod animation;
+mod field;
 mod icon;
 
 pub use animation::*;
+pub use field::*;
 pub use icon::*;
