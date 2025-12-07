01180fee6cea21cad311432d0ec5e84fb27e4765
Andreas Krath <andreas.krath+github@gmail.com>
2025-12-07 20:11:03 +0100

    crates/types: modify `Spell` to only represent valid spells
    in other words, you cannot have a `Spell` object without it being valid
    (this is not techincally true because of id, not sure about that atm)

--- crates/types/src/spell/mod.rs
+++ crates/types/src/spell/mod.rs
@@ -23,45 +23,20 @@ pub use shape::SpellShape;
 pub struct Spell {
     pub id: Option<Uuid>,
     pub name: String,
-    pub school: Option<MagicSchool>,
-    pub level: Option<SpellLevel>,
+    pub school: MagicSchool,
+    pub level: SpellLevel,
     pub verbal: bool,
     pub somatic: bool,
     pub material: Option<String>,
     pub material_consumed: bool,
     pub ritual: bool,
     pub concentration: bool,
-    pub casting_time: Option<SpellCastingTime>,
-    pub duration: Option<SpellDuration>,
-    pub range: Option<SpellRange>,
-    pub area: Option<SpellArea>,
-    pub shape: Option<SpellShape>,
+    pub casting_time: SpellCastingTime,
+    pub duration: SpellDuration,
+    pub range: SpellRange,
+    pub area: SpellArea,
+    pub shape: SpellShape,
     pub description: String,
     pub at_higher_levels: Option<String>,
     pub classes: Vec<Class>,
-}
-
-impl Default for Spell {
-    fn default() -> Self {
-        Self {
-            id: None,
-            name: String::new(),
-            school: None,
-            level: None,
-            verbal: false,
-            somatic: false,
-            material: None,
-            material_consumed: false,
-            ritual: false,
-            concentration: false,
-            casting_time: None,
-            duration: None,
-            range: None,
-            area: None,
-            shape: None,
-            description: String::new(),
-            at_higher_levels: None,
-            classes: Vec::new(),
-        }
-    }
 }
