13392c7d23cc001e5aaddd6117c6679ab4871b83
Andreas Krath <andreas.krath+github@gmail.com>
2025-12-28 23:22:45 +0100

    crates/widgets: add `heading` text helper

--- crates/widgets/src/text.rs
+++ crates/widgets/src/text.rs
@@ -5,7 +5,15 @@ use iced::widget::{
 use style::Typography;
 
 pub fn paragraph<'a>(text: impl IntoFragment<'a>) -> Text<'a> {
+    widget::text(text).style(style::text::default)
+}
+
+pub fn heading<'a>(text: impl IntoFragment<'a>) -> Text<'a> {
     widget::text(text)
+        .size(14)
+        .font(Typography::heading_bold())
+        .shaping(Shaping::Advanced)
+        .style(style::text::default)
 }
 
 pub fn muted_heading<'a>(text: impl IntoFragment<'a>) -> Text<'a> {
