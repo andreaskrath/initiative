f3d509cf74cbcc0d7c0490111a864349dab30d95
Andreas Krath <andreas.krath+github@gmail.com>
2025-12-31 16:37:34 +0100

    crates/style: move some constants to root instead of button module
    some of these things apply to other widget's styling as well

--- crates/style/src/button/mod.rs
+++ crates/style/src/button/mod.rs
@@ -1,29 +1,13 @@
 use iced::{
-    Border, Color, Shadow, Theme, Vector,
-    border::Radius,
+    Theme,
     widget::button::{Status, Style},
 };
+
+use crate::{NO_BORDER, NO_SHADOW};
 
 pub mod background;
 pub mod danger;
 pub mod primary;
-
-const NO_SHADOW: Shadow = Shadow {
-    color: Color::TRANSPARENT,
-    offset: Vector::ZERO,
-    blur_radius: 0.0,
-};
-
-const NO_BORDER: Border = Border {
-    color: Color::TRANSPARENT,
-    width: 0.0,
-    radius: Radius {
-        top_left: 0.0,
-        top_right: 0.0,
-        bottom_right: 0.0,
-        bottom_left: 0.0,
-    },
-};
 
 fn base(theme: &Theme, _status: Status) -> Style {
     let palette = theme.palette();
--- crates/style/src/lib.rs
+++ crates/style/src/lib.rs
@@ -6,8 +6,28 @@ pub mod text;
 pub mod theme;
 pub mod typography;
 
+use iced::{Border, Color, Shadow, Vector, border::Radius};
 pub use theme::ThemeVariant;
 pub use typography::Typography;
 
 /// A scale used to adjust alpha for muted styles.
 const MUTED_SCALE: f32 = 0.6;
+
+/// A `Shadow` instance that is transparent, with no offset and no radius.
+const NO_SHADOW: Shadow = Shadow {
+    color: Color::TRANSPARENT,
+    offset: Vector::ZERO,
+    blur_radius: 0.0,
+};
+
+/// A `Border` instance that is transparent, with zero width and no radius.
+const NO_BORDER: Border = Border {
+    color: Color::TRANSPARENT,
+    width: 0.0,
+    radius: Radius {
+        top_left: 0.0,
+        top_right: 0.0,
+        bottom_right: 0.0,
+        bottom_left: 0.0,
+    },
+};
