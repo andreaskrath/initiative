e359f4ca29e837642170e9c31517f289c6cf2d3d
Andreas Krath <andreas.krath+github@gmail.com>
2026-04-18 09:46:07 +0200

    crates/widgets: remove unused error variant
    also removes `thiserror` usage in crate

--- crates/widgets/src/widgets.rs
+++ crates/widgets/src/widgets.rs
@@ -5,25 +5,7 @@ pub mod multi_text_input;
 
 use style::theme::Theme;
 
-use thiserror::Error;
 
-#[derive(Debug, Error)]
-pub(crate) enum ValidationError {
-    #[error("Must be specified.")]
-    Required,
-    #[error("Must be longer than {0} characters.")]
-    Short(usize),
-    #[error("Must be shorter than {0} characters.")]
-    Long(usize),
-    #[error("Must be between {0} and {1} characters.")]
-    Between(usize, usize),
-    #[error("Must be less than {0}.")]
-    LessThan(i32),
-    #[error("Must be greater than {0}.")]
-    GreaterThan(i32),
-    #[error("Must be between {0} and {1}.")]
-    BetweenValue(i32, i32),
-}
 
 /// Type alias for [`iced::Element`] to use custom Theme from the `style` crate.
 pub type Element<'a, Message> = iced::Element<'a, Message, Theme>;
